31b962081dd7dbb829a4bae04caad8e2b2af9739
Kayo Hamid <kayo@bvcupons.com.br>
2014-02-11 09:53:59 -0400

    [ADJUST] Alterando para HTML5
    Signed-off-by: Kayo Hamid <kayo@bvcupons.com.br>

--- topo.php
+++ topo.php
@@ -27,15 +27,15 @@ $count = mysql_num_rows($sql);
 ?>
 
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 
 <head>
 
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
 <title>Catálogo de peças - BL Auto Peças</title>
+    
+    <meta charset="utf-8">
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
