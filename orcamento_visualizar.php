c9e96c0cfe1ea79a07b278e1cdb7b67a3a91ae21
sidneiweber <sidnei.weber@gmail.com>
2015-07-10 15:54:54 -0300

    Alterado layout do orçamento

--- orcamento_visualizar.php
+++ orcamento_visualizar.php
@@ -9,7 +9,7 @@ $db = mysql_select_db("$selecione")
 <?php
 if(isset($_GET['idOrcamento'])){
 	$idOrcamento = $_GET['idOrcamento'];
-	$query = "SELECT A.NOME, A.ENDERECO, A.TELEFONE, B.idOrcamento, DATE_FORMAT(B.dataHora, '%d/%m/%Y %H:%i:%s') as dataHora FROM clientes A INNER JOIN orcamento B on A.idCliente = B.idCliente WHERE B.idOrcamento = ".$idOrcamento;
+	$query = "SELECT A.NOME, A.ENDERECO, A.TELEFONE, B.idOrcamento, DATE_FORMAT(B.dataHora, '%d/%m/%Y %H:%i') as dataHora FROM clientes A INNER JOIN orcamento B on A.idCliente = B.idCliente WHERE B.idOrcamento = ".$idOrcamento;
 	
 	$sql = mysql_query($query);
 	if($res=mysql_fetch_array($sql)){
@@ -18,141 +18,99 @@ if(isset($_GET['idOrcamento'])){
 		$telefone = $res[2];
 		$idOrcamento = $res[3];
 		$dataHora = $res[4];
-        $i++;
-        $css = ($i % 2 == 0) ? 'style="background: #FFF;"' : 'style="background: #e7e7e7;"';
 	}
 }
 ?>
+<?php require ("functions.php");?>
 
 <html>
 	<head>
         <link href="css/estilo_orcamento.css" rel="stylesheet" type="text/css" />
-       	 <style type="text/css">
-			@media print {
-			  .noprint { display: none; }
-			}
-  		</style>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+        <style type="text/css">
+		  @media print {
+		  .noprint { display: none; }
+		  }
+        </style>
     </head>
     <body>
-    	<form>
-            <table width = "650px" border = "1">
-                <tr>
-                    <td>
-                        <table width = "100%" border="0">
-                            <tr>
-                                <td width = "30%">
-<h2>
-Branco Auto Pecas
-</h2>
-                                <!--    <img src="images/logo2.png">-->
-                                </td>
-                                <td width = "40%" align="center">
-                                    Av. Sao Leopoldo 270
-                                    <br>
-                                    Sala 5 - Campo Bom
-                                    <br>
-                                    3038-1292
-                                </td>
-                                <td width = "20%" valign = "top" align="center">
-                                    Orçamento nº
-                                    <br>
-                                    <b><?php echo $idOrcamento; ?></b>
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-                
-                <tr >
-                    <td>
-                        Cliente: <?php echo $nome; ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <table width = "100%" >
-                            <tr <?php echo $css ?>>
-                                <td>
-                                    Código
-                                </td>
-                                <td align = "center" width="80%">
-                                    Descrição
-                                </td>
-                                <td>
-                                    Quantidade
-                                </td>
-                                 <td>
-                                    Valor
-                                </td>
-                                 <td>
-                                    Total
-                                </td>
-                            </tr>
-                            
+
+<div class="container">
+        <div class="col-xs-12">
+            <div class="invoice-title">
+                <img src="images/logo_nota.png"><h3 class="pull-right">Ordem #<?php echo $idOrcamento; ?></b></h3>
+            </div>
+            <hr>
+            <div class="row" >
+                <div class="col-xs-6">
+                    
+                    <strong>Cliente:</strong> <?php echo $nome; ?><br>
+                    
+                </div>
+                <div class="col-xs-6 text-right">
+                   
+                        <strong>Data ordem:</strong> <?php echo $dataHora ?><br><br>
+                   
+                </div>
+            </div>
+            
+          
+    </div>
+    
+    
+                <div class="panel-body">
+                        <table class="table table-condensed" >
+                            <thead>
+                                <tr bgcolor="#CCC">                                    
+                                    <td><font size="2"><strong>Código</strong></td>
+                                    <td class="text-center"><font size="2"><strong>Descrição</strong></td>
+                                    <td class="text-center"><font size="2"><strong>Qtd</strong></td>
+                                    <td class="text-center"><font size="2"><strong>Preço</strong></td>
+                                    <td class="text-right"><font size="2"><strong>Total</strong></td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- foreach ($order->lineItems as $line) or some such thing here -->
+                              <tr>   
+
                             <?php
                                 $sql = mysql_query("SELECT A.descricao, B.quantidade, B.preco, A.codigo, A.produto FROM produtos A inner join orcamento B on A.id = B.idProduto WHERE B.idOrcamento = ".$idOrcamento." ORDER BY A.descricao ASC")
                        or die(mysql_error());
                                 $total = 0;
                                 while($res=mysql_fetch_array($sql)) {
-                                    echo '<tr><td valign=\'top\'><font size="2">'.$res[3].'</td><td valign=\'top\'><font size="2">'.$res[4].' '.$res[0].'</td><td valign=\'top\' align=\'center\'><font size="2">'.$res[1].'</td><td valign=\'top\' align=\'center\'><font size="2">'.number_format($res[2], 2).'</td><td valign=\'top\' align=\'center\'><font size="2">'.number_format($res[1]*$res[2], 2).'</td></tr>';
+                                    echo '<tr><td valign=\'top\'><font size="1">'.$res[3].'</td>
+                                    <td valign=\'top\'><font size="1">'.limita_caracteres($res[4].' '.$res[0], 65).'</td>
+                                    <td valign=\'top\' align=\'center\'><font size="1">'.$res[1].'</td>
+                                    <td valign=\'top\'><font size="1">'.number_format($res[2], 2).'</td>
+                                    <td valign=\'top\'><font size="1">'.number_format($res[1]*$res[2], 2).'</td>
+                                    </tr>';
+                                    
                                     $total += $res[1]*$res[2];
                                 }
                             ?>
-                            
-                            <tr>
-                                <td colspan = "5">
-                                
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colspan = "5" align = "right">
-                                    Total:
-                                </td>
-                                <td>
-                                    <?php echo number_format($total, 2) ?>
-                                </td>
-                             </tr> 
-                         </table>
-                     </td>
-                 </tr>
-                 <tr>
-                    <td>
-                        <table width = "100%">
-                            <tr>
-                                <td width = "25%" valign = "top"> 
-                                
-                                    <?php echo $dataHora ?> 
-                                </td>
-                                <td width = "75%" align = "right"> 
-                                    <br><br>
-                                    <table width = "100%" border = "0">
-                                        <tr>
-                                            <td align = "right">
-                                                _______________________________________________
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td align = "right">
 
-                                                <?php echo $nome; ?>
-                                            </td>
-                                        </tr>
-                                    </table>
-                                </td>
-                            </tr>
+                                <tr>
+                                    <td class="thick-line"></td>
+                                    <td class="thick-line"></td>
+                                    <td class="thick-line"></td>
+
+                                    <td class="thick-line text-right"><strong>Total</strong></td>
+                                    <td class="thick-line text-right"><?php echo number_format($total, 2) ?></td>
+                                </tr>
+                        
+                            </tbody>
+                            
                         </table>
-                    </td>
-                 </tr> 	
-            </table>
-            <table width = "595px" border = "0">
-                 <tr>
-                    <td align = "center" class='noprint'>
-                        <br><br>
-                        <input type="button" value="Imprimir" onclick=javascript:window.print()> 
-                    </td>
-                </tr>  
-            </table>
+                  
+                </div>
+    
        </form>
-       
+
+            <table>               
+            <td align = "center" class='noprint'>
+            <input type="button" value="Imprimir" onclick=javascript:window.print()> 
+            </td>
+            </table>
+           
     </body>
 </html>
